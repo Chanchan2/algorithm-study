5b5dc4cc09a7a39546ede9e78191ea5e43017a4e
공은찬_T2009 <sh2cdo@gmail.com>
2021-10-14 22:04:23 +0900

    day 6

--- book/ch10/kec/real_question_10_4.py
+++ book/ch10/kec/real_question_10_4.py
@@ -1,4 +1,5 @@
-import deque
+from collections import deque
+import copy
 
 N = int(input())
 times = [0] * (N + 1)
@@ -8,16 +9,28 @@ q= deque()
 
 for i in range(1, N + 1) :
     temp = list(map(int, input().split()))
-    times[i] = temp[0]
+    times[i] = temp[0]      #강의 시간 저장
 
     if len(temp) > 2 :
         for precourse in temp[1 : -1] :
-            graph[precourse].append(i)
+            graph[precourse].append(i)      #선수과정에 상위과정 저장
             indegree[i] += 1
     else :
-        q.append(i)
+        q.append(i)   # 진입차수 0인 경우 저장
 
-while q :
-    now = q.popleft()
-    for i in graph[now] :
-        times[i] = 
+def topology_sort() :
+    result = copy.deepcopy(times)
+
+    while q :
+        now = q.popleft()
+        for i in graph[now] :
+            result[i] = max(result[i], result[now] + times[i])
+            indegree[i] -= 1
+            if indegree[i] == 0 :
+                q.append(i)
+        # print(q)
+
+    for i in range(1, N + 1) :
+        print(result[i])
+
+topology_sort()
